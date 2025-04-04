5bf6dea094a3e1f598d961eb34378f475880328b
Dmitry Kiselev <mister.garry75@yandex.ru>
2025-04-04 13:19:54 +0300

    Фз-11. add-friends-likes

--- src/main/java/ru/yandex/practicum/filmorate/service/FilmService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/FilmService.java
@@ -4,14 +4,17 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import ru.yandex.practicum.filmorate.exception.FilmNotFoundException;
+import ru.yandex.practicum.filmorate.exception.UserNotFoundException;
 import ru.yandex.practicum.filmorate.exception.ValidationException;
 import ru.yandex.practicum.filmorate.model.Film;
+import ru.yandex.practicum.filmorate.model.User;
 import ru.yandex.practicum.filmorate.storage.film.FilmStorage;
 import ru.yandex.practicum.filmorate.storage.user.UserStorage;
 
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
+import java.util.Optional;
 
 @Slf4j
 @Service
@@ -27,27 +30,48 @@ public class FilmService {
 
     public void addLike(Long filmId, Long userId) {
         log.info("Добавление лайка фильму {} от пользователя {}", filmId, userId);
-        try {
-            Film film = filmStorage.getFilmById(filmId);
-            userStorage.getUserById(userId);
 
-            if (film.getLikes().contains(userId)) {
-                log.warn("Пользователь {} уже ставил лайк фильму {}", userId, filmId);
-                throw new ValidationException("Пользователь уже лайкнул этот фильм");
-            }
-
-            film.getLikes().add(userId);
-            log.debug("Текущие лайки фильма {}: {}", filmId, film.getLikes());
-        } catch (RuntimeException e) {
-            throw new FilmNotFoundException("Фильм не найден");
+        // Проверяем существование фильма
+        Optional<Film> filmOptional = Optional.ofNullable(filmStorage.getFilmById(filmId));
+        if (filmOptional.isEmpty()) {
+            throw new FilmNotFoundException("Фильм с id " + filmId + " не найден");
         }
+        Film film = filmOptional.get();
+
+        // Проверяем существование пользователя
+        Optional<User> userOptional = Optional.ofNullable(userStorage.getUserById(userId));
+        if (userOptional.isEmpty()) {
+            throw new UserNotFoundException("Пользователь с id " + userId + " не найден");
+        }
+
+        // Проверяем, не ставил ли пользователь лайк ранее
+        if (film.getLikes().contains(userId)) {
+            log.warn("Пользователь {} уже ставил лайк фильму {}", userId, filmId);
+            throw new ValidationException("Пользователь уже лайкнул этот фильм");
+        }
+
+        // Добавляем лайк
+        film.getLikes().add(userId);
+        log.debug("Текущие лайки фильма {}: {}", filmId, film.getLikes());
     }
 
     public void removeLike(Long filmId, Long userId) {
-        Film film = filmStorage.getFilmById(filmId);
-        userStorage.getUserById(userId); // Проверяем что пользователь существует
+        // Проверяем существование фильма
+        Optional<Film> filmOptional = Optional.ofNullable(filmStorage.getFilmById(filmId));
+        if (filmOptional.isEmpty()) {
+            throw new FilmNotFoundException("Фильм с id " + filmId + " не найден");
+        }
+        Film film = filmOptional.get();
+
+        // Проверяем существование пользователя
+        Optional<User> userOptional = Optional.ofNullable(userStorage.getUserById(userId));
+        if (userOptional.isEmpty()) {
+            throw new UserNotFoundException("Пользователь с id " + userId + " не найден");
+        }
+
+        // Удаляем лайк и проверяем его существование
         if (!film.getLikes().remove(userId)) {
-            throw new FilmNotFoundException("Лайк от пользователя " + userId + " не найден для фильма " + filmId);
+            throw new ValidationException("Лайк от пользователя " + userId + " не найден для фильма " + filmId);
         }
     }
 
